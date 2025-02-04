becabf54a77a5398c9f029974c904baa29f640b7
Song-Gi-Sung <wlswlek123@naver.com>
2025-02-04 14:30:50 +0900

    git init

---
+++ gateway/src/test/java/com/taxi/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxi.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
